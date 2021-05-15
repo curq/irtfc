1a2954a0fc4170bae1625090f9a10e259f39ea68
nonSpecificUser7 <gsirazh@gmail.com>
2021-05-15 17:42:05 +0500

    add basic login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,22 @@
+import { useHistory } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || email === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login — Instagram';
+  }, []);
+
   return <p>I am the login page</p>;
 }
