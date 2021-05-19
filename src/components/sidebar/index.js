57c3cba8ff58d7834582f008f2311ff2c2a319ff
nonSpecificUser7 <gsirazh@gmail.com>
2021-05-19 20:35:12 +0500

    update sidebar

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,18 @@
+import useUser from '../../hooks/use-user';
+import Suggestions from './suggestions';
+import User from './user';
+
+export default function Sidebar() {
+  const {
+    user: { fullname, username, userId }
+  } = useUser();
+
+  console.log('x', fullname, username, userId);
+
+  return (
+    <div className="p-4">
+      <User username={username} fullname={fullname} />
+      <Suggestions userId={userId} />
+    </div>
+  );
+}
