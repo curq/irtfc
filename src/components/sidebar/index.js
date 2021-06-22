d6086dd06b314e39556768cbd11ef2b41404f2aa
nsuser <npuser7@gmail.com>
2021-06-22 19:53:12 +0500

    finish sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,15 +4,15 @@ import User from './user';
 
 export default function Sidebar() {
   const {
-    user: { fullname, username, userId }
+    user: { fullname, username, userId, following, docId }
   } = useUser();
 
-  // console.log('x', fullname, username, userId);
+  // console.log('x', docId);
 
   return (
     <div className="p-4">
       <User username={username} fullname={fullname} />
-      <Suggestions userId={userId} />
+      <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
     </div>
   );
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -19,3 +19,46 @@ export async function getUserByUserId(userId) {
 
   return user;
 }
+
+export async function getSuggestedProfiles(userId, following) {
+  const result = await firebase.firestore().collection('users').limit(10).get();
+  // console.log(result);
+  return result.docs
+    .map((user) => ({
+      ...user.data(),
+      docId: user.id
+    }))
+    .filter((profile) => profile.userId !== userId && !following.includes(profile.userId));
+}
+
+// updateLoggedInUserFollowing, updateFollowedUserFollowers
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId, // logged in user that will follow
+  profileId, // the user that is requested to follow
+  isFollowing // is logged user already following requested user?
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowing ? FieldValue.arrayRemove(profileId) : FieldValue.arrayUnion(profileId)
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId, // logged in user that will follow
+  loggedInUserDocId, // the user that is requested to follow
+  isFollowing // is logged user already following requested user?
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(profileDocId)
+    .update({
+      followers: isFollowing
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId)
+    });
+}
